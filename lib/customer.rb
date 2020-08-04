d31e0e989812f2502c1a556ef6357df8339916c6
Zainab Omar <zainab.alshaikhli01@gmail.com>
2020-08-04 21:02:00 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -14,6 +14,6 @@ class Customer
     Meal.new(waiter, self, total, tip)
   end
   def meals
-    Meal.all.select {|meal| meal.name = self}
+    Meal.all.select {|meal| meal.customer = self}
   end
 end
